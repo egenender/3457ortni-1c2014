352532d0312c572ec6771363c79b3efead0f8390
egenender <egenender@gmail.com>
2014-06-04 13:43:54 -0300

    todas las clouds en GNS3. Todas los comandos tap. Proximo paso: hacerlos script

--- scripts/server/finalizarVPNs.sh
+++ scripts/server/finalizarVPNs.sh
@@ -2,3 +2,12 @@
 
 killall openvpn
 openvpn --rmtun --dev tap0
+openvpn --rmtun --dev tap1
+openvpn --rmtun --dev tap2
+openvpn --rmtun --dev tap3
+openvpn --rmtun --dev tap4
+openvpn --rmtun --dev tap5
+openvpn --rmtun --dev tap6
+openvpn --rmtun --dev tap7
+openvpn --rmtun --dev tap8
+openvpn --rmtun --dev tap9
