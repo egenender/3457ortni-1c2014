2c55cade8ee3f437b643c664bad3bbf85c64ef39
egenender <egenender@gmail.com>
2014-06-06 12:38:39 -0300

    scripts tienen el ruteo estatico

--- scripts/hosts/levantarDNS2.sh
+++ scripts/hosts/levantarDNS2.sh
@@ -3,3 +3,7 @@
 IP=$1
 
 openvpn --remote $IP --port 1205 --dev tap5 --ifconfig 10.31.25.194 255.255.255.224 10.134.5.140
+
+sleep 5
+
+route add -net 0.0.0.0 netmask 0.0.0.0 gw 10.31.25.193 metric 1
--- scripts/hosts/levantarHostC.sh
+++ scripts/hosts/levantarHostC.sh
@@ -3,3 +3,33 @@
 IP=$1
 
 openvpn --remote $IP --port 1202 --dev tap2 --ifconfig 20.86.15.34 255.255.255.224 10.134.5.140
+
+sleep 5
+
+route add -net 201.158.15.0  netmask 255.255.255.128 gw 20.86.15.34 metric 1
+route add -net 20.86.15.0 netmask 255.255.255.224 gw 20.86.15.34 metric 1
+route add -net 10.31.25.128 netmask 255.255.255.240 gw 20.86.15.34 metric 1
+route add -net 20.64.73.0 netmask 255.255.255.0 gw 20.86.15.33 metric 1
+route add -net 10.31.25.0 netmask 255.255.255.128 gw 20.86.15.33  metric 1
+route add -net 10.31.25.152 netmask 255.255.255.252 gw 20.86.15.33  metric 1
+route add -net 10.31.25.156 netmask 255.255.255.252 gw 20.86.15.33  metric 1
+route add -net 151.40.3.192 netmask 255.255.255.252 gw 20.86.15.34  metric 1
+route add -net 151.40.3.196 netmask 255.255.255.252 gw 20.86.15.34 metric 1
+route add -net 151.40.3.200 netmask 255.255.255.252 gw 20.86.15.34 metric 1
+route add -net 151.40.3.204 netmask 255.255.255.252 gw 20.86.15.34 metric 1
+route add -net 151.40.3.208 netmask 255.255.255.252 gw 20.86.15.34 metric 1
+route add -net 151.40.3.212 netmask 255.255.255.252 gw 20.86.15.34 metric 1
+route add -net 151.40.3.216 netmask 255.255.255.252 gw 20.86.15.34 metric 1
+route add -net 151.40.3.220 netmask 255.255.255.252 gw 20.86.15.34 metric 1
+route add -net 151.40.3.224 netmask 255.255.255.252 gw 20.86.15.34 metric 1
+route add -net 151.40.3.228 netmask 255.255.255.252 gw 20.86.15.34 metric 1
+route add -net 20.86.15.64 netmask 255.255.255.192 gw 20.86.15.34 metric 1
+route add -net 10.31.25.144 netmask 255.255.255.248 gw 20.86.15.33 metric 1
+route add -net 20.26.29.0 netmask 255.255.255.0 gw 20.86.15.34 metric 1
+route add -net 20.86.15.128 netmask 255.255.255.128 gw 20.86.15.33 metric 1
+route add -net 10.31.25.192 netmask 255.255.255.224 gw 20.86.15.33 metric 1
+route add -net 10.31.25.160 netmask 255.255.255.252 gw 20.86.15.33 metric 1
+route add -net 10.31.25.164 netmask 255.255.255.252 gw 20.86.15.33 metric 1
+route add -net 10.31.25.168 netmask 255.255.255.252 gw 20.86.15.33 metric 1
+	
+	
