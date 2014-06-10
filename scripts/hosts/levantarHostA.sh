b905ccd396d5b423a0cc72888b9215abdd4a0451
egenender <egenender@gmail.com>
2014-06-10 00:18:07 -0300

    arreglo del puerto

--- scripts/hosts/levantarHostA.sh
+++ scripts/hosts/levantarHostA.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#./finalizarVPNs.sh
+./finalizarVPNs.sh
 
 IP=$1
 
-openvpn --remote $IP --port 1210 --dev tap10 --ifconfig 201.158.15.2 255.255.255.128 10.134.5.140 &
+openvpn --remote $IP --port 1200 --dev tap0 --ifconfig 201.158.15.2 255.255.255.128 10.134.5.140 &
 
 sleep 25
 
